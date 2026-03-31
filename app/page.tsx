50617c444fbf9f7d371f9d11f4b3c175bc9aded5
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2026-03-31 15:40:49 +0100

    Write the initial kitty component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+function KittyCard() {
+  return (
+    <>
+      <img src="kitten_photo.jpeg" alt="photo of a kitten" />
+      <h2>Kitty Name</h2>
+      <p>This is some information about the kitty. Like what are they famous for and stuff.</p>
+    </>
+  )
+}
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
-      <p>Kitty</p>
+      <h1>Amazing Kitties</h1>
+      <KittyCard />
     </>
   );
 }
